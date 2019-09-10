b0b7050157ae9183d75eefe05091597334c982d8
George <admin@georgesimos.com>
2019-09-10 21:38:23 +0300

    Done

---
+++ modelEvaluate.js
@@ -0,0 +1,51 @@
+export const classNames = [
+  'Zero',
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine'
+];
+
+export function doPrediction(model, data, testDataSize = 500) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([
+    testDataSize,
+    IMAGE_WIDTH,
+    IMAGE_HEIGHT,
+    1
+  ]);
+  const labels = testData.labels.argMax([-1]);
+  const preds = model.predict(testxs).argMax([-1]);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+export async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = { name: 'Accuracy', tab: 'Evaluation' };
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+export async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = { name: 'Confusion Matrix', tab: 'Evaluation' };
+  tfvis.render.confusionMatrix(
+    container,
+    { values: confusionMatrix },
+    classNames
+  );
+
+  labels.dispose();
+}
